dc058e1209d2c10c10dbf6f47da4cd78ca76896e
Shamil Mammadov <shamil@miro.com>
2024-01-10 10:35:19 +0100

    init commit

--- src/Player.ts
+++ src/Player.ts
@@ -1,11 +1,14 @@
+type GameState = { current_buy_in: number };
+
 export class Player {
-  public betRequest(gameState: any, betCallback: (bet: number) => void): void {
-    betCallback(0);
+  public betRequest(
+    gameState: GameState,
+    betCallback: (bet: number) => void
+  ): void {
+    betCallback(Math.min(gameState.current_buy_in * 2, 100));
   }
 
-  public showdown(gameState: any): void {
-
-  }
-};
+  public showdown(gameState: GameState): void {}
+}
 
 export default Player;
