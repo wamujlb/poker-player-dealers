8ac1775535a4fa1abe7876bd2332b280bc063d3f
Shamil Mammadov <shamil@miro.com>
2024-01-10 10:40:40 +0100

    added types

--- src/Player.ts
+++ src/Player.ts
@@ -1,4 +1,26 @@
-type GameState = { current_buy_in: number };
+type PlayerItem = {
+  name: string;
+  stack: number;
+  status: string;
+  bet: number;
+  hole_cards: [];
+  version: string;
+  id: number;
+};
+
+type GameState = {
+  players: PlayerItem[];
+  tournament_id: string;
+  game_id: string;
+  round: number;
+  bet_index: number;
+  small_blind: number;
+  orbits: number;
+  dealer: number;
+  community_cards: [];
+  current_buy_in: number;
+  pot: number;
+};
 
 export class Player {
   public betRequest(
